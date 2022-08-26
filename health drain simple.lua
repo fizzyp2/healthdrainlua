ec2d16449e78a1ee3356e0381858c6b7da8a962f
fizzy <108989676+fizzyp2@users.noreply.github.com>
2022-08-26 13:57:15 +0100

    Update health drain simple.lua

--- health drain simple.lua
+++ health drain simple.lua
@@ -1,6 +1,6 @@
-function opponentNoteHit()
-	health = getProperty('health')
-	if getProperty('health') > 0.1 then
-	setProperty('health', health- 0.02);
+function opponentNoteHit() --basically this is when the opponent hits the note
+	health = getProperty('health') --health property
+	if getProperty('health') > 0.1 then --gets the health then minus it by whatever idk
+	setProperty('health', health- 0.02); --opponent hit notes health goes downy
 	end
 	end
